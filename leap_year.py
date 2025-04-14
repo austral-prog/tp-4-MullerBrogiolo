ca6538b79baa32f0ac234bd8708686bb39c5929d
MullerBrogiolo <atbrogiolo@mail.austral.edu.ar>
2025-04-14 17:04:41 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,9 +1,8 @@
 def leap_year():
-# Defino la variable "año", para luego aplicarla al "if":
-
+    # Defino la variable "año", para luego aplicarla al "if":
     a = int(input("Año: "))
 
-    if (a % 4 == 0) and (a % 100 != 0 or a % 400 == 0):
+    if (a % 4 == 0 and a % 100 != 0) or (a % 400 == 0):
         print(f"El {a} es BISIESTO")
     else:
         print(f"El {a} NO es BISIESTO")
